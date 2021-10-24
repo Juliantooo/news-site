d9e89df5ef21644512d8064e7e91d87bce60436b
Fajar Julianto <slashcrackdie@gmail.com>
2021-10-24 18:25:11 +0700

    add routes

---
+++ src/router/routes.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Home = React.lazy(()=> import('../views/home'));
+const Covid = React.lazy(()=> import('../views/covid'));
+const Indonesia = React.lazy(()=> import('../views/indonesia'));
+const Programing = React.lazy(()=> import('../views/programing'));
+const Saved = React.lazy(()=> import('../views/saved'));
+
+const routes = [
+    {
+        path: '/',
+        name : 'Home',
+        exact:true,
+        component : Home
+    },
+    {
+        path: '/programing',
+        name : 'Programing',
+        exact:true,
+        component : Programing
+    },
+    {
+        path: '/indonesia',
+        name : 'Indonesia',
+        exact:true,
+        component : Indonesia
+    },
+    {
+        path: '/covid',
+        name : 'Covid',
+        exact:true,
+        component : Covid
+    },
+    {
+        path: '/saved',
+        name : 'Save',
+        exact:true,
+        component : Saved
+    },
+];
+
+export default routes
