7202c5c2eb143937aa8e60532f9e69af13c28713
Fajar Julianto <slashcrackdie@gmail.com>
2021-10-24 18:25:43 +0700

    create http service

---
+++ src/service/http.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const baseUrl = process.env.REACT_APP_BASE_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const parseResponse = (response)=>{
+    const responseBody = response.data
+
+    return{
+        isError : responseBody.status !== 'ok',
+        data : responseBody.articles
+    }
+}
+
+export const http = (method, url) => new Promise((resolve, reject) => {
+  axios({
+    method,
+    url:`${baseUrl}${url}&apiKey=${apiKey}`,
+  })
+    .then((response) => {
+      resolve(parseResponse(response));
+    })
+    .catch((errors) => {
+      reject(errors);
+    });
+});
+
+
