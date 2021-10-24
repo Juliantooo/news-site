bace9004cf7064b86ea282efc60cd852c4d0e4b7
Fajar Julianto <slashcrackdie@gmail.com>
2021-10-24 18:23:54 +0700

    add component footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-gray-800 p-8 flex flex-row justify-between">
+            <p className="text-2xl font-medium">GNews</p>
+            <small>Copyright © 2021 GNews. All rights reserved.</small>
+        </div>
+    )
+}
+
+export default Footer
