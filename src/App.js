ab4d1a70994911cf48ad56dc1fc22d6d4a2243f1
Fajar Julianto <slashcrackdie@gmail.com>
2021-10-24 18:22:21 +0700

    update index config tailwind stuff and react slick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import News from './Component/News';
-import { NewsApiAkses } from "./API/NewsApi"
+import './index.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {Default} from './layout/default';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import routes from './router/routes';
+import React,{ Suspense } from 'react';
 
 function App() {
+
+  const loading = (
+    <div className="pt-3 text-center">
+      <div className="sk-spinner sk-spinner-pulse"></div>
+    </div>
+  )
+
   return (
-    <NewsApiAkses>
-      <News />
-    </NewsApiAkses>
+    <Router>
+      <Suspense fallback={loading}>
+        <Switch>
+          <Default>
+            {
+              routes.map((route,idx)=>{
+                return (
+                    route.component && (
+                      <Route exact={route.exact} key={idx} path={route.path} name={route.name} render={props => <route.component {...props} />}/>
+                    )
+                  )
+              })
+            }
+          </Default>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
