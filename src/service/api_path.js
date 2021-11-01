a9032b7d5febdea0f0cd64e6051dffa3779df274
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-01 20:22:49 +0700

    add path to get covid news

--- src/service/api_path.js
+++ src/service/api_path.js
@@ -1,2 +1,3 @@
-const region = 'id'
-export const TOP_HEADLINES = `top-headlines?country=${region}`
+const region = 'id';
+export const TOP_HEADLINES = `top-headlines?country=${region}`;
+export const COVID_19 = `everything?q=covid&from=2021-10-01`;
