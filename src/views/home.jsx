6df5d534b793f1a6739b4a7e7cbcc6a2d7869b6c
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-01 20:26:03 +0700

    fix carousel and responsive

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -17,10 +17,10 @@ const settings = {
     cssEase: "linear",
     pauseOnHover: true,
     arrows: false,
-    className: "slider variable-width",
+    className: "slider px-2 cursor-pointer",
     customPaging: () => (
-        <span className="w-6 h-1.5 bg-red-500 absolute bottom-8 sm:bottom-16 lg:bottom-6 xl:bottom-8 cursor-pointer hover:bg-red-600"></span>
-      )
+        <span className="w-6 h-1.5 bg-red-500 absolute bottom-8 cursor-pointer hover:bg-red-600"></span>
+    )
   };
 
 const Home = () => {
@@ -43,14 +43,14 @@ const Home = () => {
 
     return (
         <div>
-            <Slider {...settings} className="w-full px-2 md:w-10/12 xl:w-9/12 h-full sm:h-80 lg:h-96 mx-auto my-5 lg:my-10">
+            <Slider {...settings}>
                 {
                     headlineNews&&headlineNews.map((news,idx)=>{
                         return(
                             <div className="relative h-full" key={idx}>
-                                <img src={news.urlToImage} alt="headline-news" className="w-full xl:h-96" />
+                                <img src={news.urlToImage} alt="headline-news" className="w-full h-60 md:h-80 lg:h-96" />
                                 <div className="bg-gray-900 bg-opacity-60 w-full h-26 absolute bottom-0 z-10 p-4 space-y-3">
-                                    <p className="lg:text-xl font-semibold">{news.title}</p>
+                                    <p className="text-sm lg:text-xl font-semibold">{news.title}</p>
                                     <p className="text-xs lg:text-small">{news.publishedAt}</p>
                                 </div>
                             </div>
@@ -60,8 +60,8 @@ const Home = () => {
             </Slider>
                 <h1 className="mx-2 text-2xl font-bold my-5">Berita Terkini</h1>
                     <hr className="mx-2 "/>
-            <section className="mx-2 flex flex-row space-x-3 flex-wrap">
-                <div className="newst-news w-full lg:w-2/3 space-y-2 mt-5">
+            <section className="mx-2 flex flex-row flex-wrap">
+                <div className="newst-news w-full lg:w-2/3 space-y-2 mt-5 p-3">
                     {
                         newstNews.length > 0 ?
                         newstNews.map((news,idx)=> <NewsCard key={idx} published={news.publishedAt} title={news.title} category="Berita Terkini" image={news.urlToImage} />)
@@ -76,7 +76,7 @@ const Home = () => {
                         </div>
                     }
                 </div>
-                <div className="popular-news w-full lg:w-1/3">
+                <div className="popular-news w-full lg:w-1/3 p-3">
                     <h1 className="text-2xl font-bold my-3">Trending</h1>
                     <div className="space-y-2">
                         {
