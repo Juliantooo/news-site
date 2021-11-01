a1402251f00db63990c99298771a14f723467d8b
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-01 20:20:41 +0700

    modify card, apply responsive card

--- src/components/newsCard.jsx
+++ src/components/newsCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const NewsCard = ({image,title,category,published}) => {
+const NewsCard = ({image,title,category,published,clickSave}) => {
     return (
-        <div className="w-full p-2 flex flex-row space-x-4 bg-gray-800 bg-opacity-20">
-            <img src={image} alt="news" className="h-32 w-48 object-cover" />
+        <div className="w-full p-2 flex flex-col md:flex-row md:space-x-4 bg-gray-800 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
+            <img src={image} alt="news" className="h-32 w-full md:w-48 object-cover" />
             <div className="flex flex-col justify-between">
-                <p className="text-lg font-bold tracking-wider mt-1">{title}</p>
+                <p className="text-md font-bold tracking-wider mt-1">{title}</p>
                 <div className="space-x-3">
                     <span className="text-red-600">{category}</span>
                     <span className="text-sm">{published}</span>
                 </div>
             </div>
+            {/* tombol save onclick={clickSave(title)} */}
         </div>
     )
 }
--- src/components/newsSideCard.jsx
+++ src/components/newsSideCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const NewsSideCard = ({image,title,category}) => {
     return (
-        <div className="w-full py-4 px-2 flex flex-row justify-between space-x-2 bg-gray-800 bg-opacity-20">
-              <img src={image} alt="news" className="h-20 w-20 object-cover mt-3" />
+        <div className="w-full p-2 flex flex-row lg:flex-col space-x-2 lg:space-x-0 justify-between  bg-gray-800 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
+            <img src={image} alt="news" className="h-20 w-28 lg:w-full md:object-cover lg:object-contain" />
             <div className="flex flex-col justify-between ">
-                <p className="tracking-wider mt-1">{title}</p>
+                <p className="tracking-wider text-sm mt-1">{title}</p>
                 <div className="space-y-3">
                     <span className="text-red-600 text-sm">{category}</span>
                 </div>
