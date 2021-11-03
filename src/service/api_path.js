1f48afff8f7f7c615e52cd069c004913deda5dcb
wind_create <nardowilli@gmail.com>
2021-11-03 20:39:49 +0700

    indonesia and programming news

--- src/service/api_path.js
+++ src/service/api_path.js
@@ -1,3 +1,5 @@
 const region = 'id';
 export const TOP_HEADLINES = `top-headlines?country=${region}`;
 export const COVID_19 = `everything?q=covid`;
+export const PROGRAMMING_NEWS = `everything?q=programming`;
+export const INDONESIA_NEWS = `top-headlines?country=${region}`
--- src/views/indonesia.jsx
+++ src/views/indonesia.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { INDONESIA_NEWS } from "../service/api_path"
+import NewsCard from "../components/newsCard";
+import Skeleton from "../components/skeleton";
+import NewsSideCard from "../components/newsSideCard";
+import { NewsHooks } from '../libs/helpers/hooks'
+import { dateParser } from "../libs/helpers/date";
 
 const Indonesia = () => {
+    const { newsData, headlineNews } = NewsHooks({ method: 'get', url: INDONESIA_NEWS });
+
+
+    const clickSave = (title) => {
+        console.log('clicked')
+        //      selectedNews = newsData.find((news)=> news.title===title)
+        //     //panggil store buat dispatch ('ADD_SAVED_NEWS',selectedNews);
+    }
+
     return (
         <div>
-            Indonesia page
+            <h1 className="mx-2 text-2xl font-bold my-5">Berita Indonesia News</h1>
+            <hr className="mx-2 " />
+            <section className="mx-2 flex flex-row flex-wrap">
+                <div className="newst-news w-full lg:w-2/3 space-y-2 mt-5 p-3">
+                    {
+                        newsData.length > 0 ?
+                            newsData.map((news, idx) => <NewsCard key={idx} published={dateParser(news.publishedAt)} title={news.title} category="Berita Terkini" image={news.urlToImage} clickSave={clickSave} />)
+                            :
+                            <div className="space-y-5 mt-5">
+                                <Skeleton />
+                                <Skeleton />
+                                <Skeleton />
+                                <Skeleton />
+                                <Skeleton />
+                                <Skeleton />
+                            </div>
+                    }
+                </div>
+                <div className="popular-news w-full lg:w-1/3 p-3">
+                    <h1 className="text-2xl font-bold my-3">Trending</h1>
+                    <div className="space-y-2">
+                        {
+                            headlineNews.length > 0 ?
+                                headlineNews.map((news, idx) => <NewsSideCard key={idx} title={news.title} category="Trending" image={news.urlToImage} />)
+                                :
+                                <div className="space-y-5 mt-5">
+                                    <Skeleton />
+                                    <Skeleton />
+                                    <Skeleton />
+                                    <Skeleton />
+                                    <Skeleton />
+                                    <Skeleton />
+                                </div>
+                        }
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
