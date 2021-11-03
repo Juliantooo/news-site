e6dd8ea944f0e88f61b9711191a07cf6eb055a85
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-03 20:15:00 +0700

    implement dummy detail news

---
+++ src/views/detailNews.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import dummy from '../assets/images/xuner.png'
+
+const DetailNews = () => {
+    return (
+        <div className="space-y-5">
+            <img src={dummy} alt="detail" className="w-full h-96 top-10 right-0" />
+            <div className="news-detail space-y-10 w-2/3 m-auto">
+                <div className="space-y-2">
+                    <h1 className="text-2xl font-bold">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor modi, veritatis quas, hic saepe, ad ut eveniet ratione autem deserunt et vel</h1>
+                    <div><span className="text-red-500">Kategori</span> | <span className="text-xs">03 November 2021</span></div>
+                    <hr />
+                </div>
+                <article className="text-lg">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam ex eaque odio ea non ad magnam maiores reprehenderit minima ipsa eligendi voluptates at doloribus rem eum, consequatur aliquam. Temporibus, sequi.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quis ipsa, veniam a ipsum assumenda quibusdam harum modi, debitis illum, et facere ex perferendis voluptates. Autem suscipit quis illo animi?
+                    <br /><br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, doloribus soluta voluptates sit numquam cumque laboriosam dolorum, amet saepe maiores magni aliquam id vero ab nihil labore cupiditate eum sequi.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, eveniet officia error consequatur est culpa architecto omnis commodi repudiandae veritatis saepe, debitis deserunt quae illum expedita perspiciatis rerum. Sit, minima.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit nam corporis laborum fuga doloremque, quidem fugit dignissimos saepe, iste ipsam qui temporibus blanditiis. Repudiandae iusto quidem corporis quibusdam assumenda iste?
+                    <br /><br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ut dicta dolores recusandae ad similique perspiciatis blanditiis voluptas laborum? Accusantium quidem voluptatibus nisi cum nesciunt consectetur magni rerum ad asperiores.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam inventore illum perferendis possimus eaque odit laboriosam culpa, fugit laudantium, necessitatibus placeat quam ab maiores optio minima at. Officiis, a doloribus.
+                    <br/> <br />
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit nam corporis laborum fuga doloremque, quidem fugit dignissimos saepe, iste ipsam qui temporibus blanditiis. Repudiandae iusto quidem corporis quibusdam assumenda iste?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ut dicta dolores recusandae ad similique perspiciatis blanditiis voluptas laborum? Accusantium quidem voluptatibus nisi cum nesciunt consectetur magni rerum ad asperiores.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam inventore illum perferendis possimus eaque odit laboriosam culpa, fugit laudantium, necessitatibus placeat quam ab maiores optio minima at. Officiis, a doloribus.
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit nam corporis laborum fuga doloremque, quidem fugit dignissimos saepe, iste ipsam qui temporibus blanditiis. Repudiandae iusto quidem corporis quibusdam assumenda iste?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ut dicta dolores recusandae ad similique perspiciatis blanditiis voluptas laborum? Accusantium quidem voluptatibus nisi cum nesciunt consectetur magni rerum ad asperiores.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam inventore illum perferendis possimus eaque odit laboriosam culpa, fugit laudantium, necessitatibus placeat quam ab maiores optio minima at. Officiis, a doloribus.
+                    <br/><br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam inventore illum perferendis possimus eaque odit laboriosam culpa, fugit laudantium, necessitatibus placeat quam ab maiores optio minima at. Officiis, a doloribus.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit nam corporis laborum fuga doloremque, quidem fugit dignissimos saepe, iste ipsam qui temporibus blanditiis. Repudiandae iusto quidem corporis quibusdam assumenda iste?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ut dicta dolores recusandae ad similique perspiciatis blanditiis voluptas laborum? Accusantium quidem voluptatibus nisi cum nesciunt consectetur magni rerum ad asperiores.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam inventore illum perferendis possimus eaque odit laboriosam culpa, fugit laudantium, necessitatibus placeat quam ab maiores optio minima at. Officiis, a doloribus.
+                </article>
+            </div>
+        </div>
+    )
+}
+
+export default DetailNews
