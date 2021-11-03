080353ce170b22e337e7ea5016f11e4b2428b432
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-03 20:12:19 +0700

    modify bgcolor and typography

--- src/components/newsCard.jsx
+++ src/components/newsCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const NewsCard = ({image,title,category,published,clickSave}) => {
     return (
-        <div className="w-full p-2 flex flex-col md:flex-row md:space-x-4 bg-gray-800 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
+        <div className="w-full p-2 flex flex-col md:flex-row md:space-x-4 bg-gray-600 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
             <img src={image} alt="news" className="h-32 w-full md:w-48 object-cover" />
             <div className="flex flex-col justify-between">
                 <p className="text-md font-bold tracking-wider mt-1">{title}</p>
                 <div className="space-x-3">
-                    <span className="text-red-600">{category}</span>
-                    <span className="text-sm">{published}</span>
+                    <span className="text-red-600 text-xs">{category}</span>
+                    <span className="text-xs">{published}</span>
                 </div>
             </div>
             {/* tombol save onclick={clickSave(title)} */}
--- src/components/newsSideCard.jsx
+++ src/components/newsSideCard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const NewsSideCard = ({image,title,category}) => {
     return (
-        <div className="w-full p-2 flex flex-row lg:flex-col space-x-2 lg:space-x-0 justify-between  bg-gray-800 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
+        <div className="w-full p-2 flex flex-row lg:flex-col space-x-2 lg:space-x-0 justify-between  bg-gray-600 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
             <img src={image} alt="news" className="h-20 w-28 lg:w-full md:object-cover lg:object-contain" />
-            <div className="flex flex-col justify-between ">
+            <div className="flex flex-col justify-between flex-grow">
                 <p className="tracking-wider text-sm mt-1">{title}</p>
                 <div className="space-y-3">
-                    <span className="text-red-600 text-sm">{category}</span>
+                    <span className="text-red-600 text-xs">{category}</span>
                 </div>
             </div>
         </div>
