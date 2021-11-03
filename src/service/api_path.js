2f14d8483082faeb5ed9723def11c04264f7a280
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-03 20:14:07 +0700

    modify api path

--- src/service/api_path.js
+++ src/service/api_path.js
@@ -1,3 +1,3 @@
 const region = 'id';
 export const TOP_HEADLINES = `top-headlines?country=${region}`;
-export const COVID_19 = `everything?q=covid&from=2021-10-01`;
+export const COVID_19 = `everything?q=covid`;
