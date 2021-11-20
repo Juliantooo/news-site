1f4d7c6ad937fade00a4f206d50101267f3424b6
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-20 19:25:39 +0700

    add feat search news

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link,useHistory } from 'react-router-dom'
 import hamburgerIcon from '../assets/images/hamburger-menu.svg'
 import logo from '../assets/images/logo gnews.png'
 
-export const Header = ({onSidebarToogle}) => {
+const Header = ({onSidebarToogle}) => {
     const history = useHistory();
+    const [keywords,setKeywords]=useState('');
 
-    const handleClick = () =>{
+    const handleClickLogo = () =>{
         history.push('/')
+    }
+
+    const handleChange = (keywords)=>{
+        setKeywords(keywords)
+    }
+
+    const handleSearchNews = () =>{
+        if(!keywords) return;
+        history.push({
+            pathname:'/search',
+            search:`?keywords=${keywords}`
+        });
+        setKeywords('');
     }
 
     return (
@@ -19,7 +33,8 @@ export const Header = ({onSidebarToogle}) => {
             justify-between
             h-16
             shadow-lg
-            px-5
+            px-2
+            sm:px-5
             md:px-16
             xl:px-28
             sticky
@@ -27,9 +42,10 @@ export const Header = ({onSidebarToogle}) => {
             z-10
             bg-gray-800
             text-white
+            tracking-wider
             "
         >
-            <img src={logo} onClick={handleClick} alt="site-logo" className="cursor-pointer w-12 h-10 md:h-12 md:w-16 rounded-full" />
+            <img src={logo} onClick={handleClickLogo} alt="site-logo" className="cursor-pointer w-12 h-10 md:h-12 md:w-16 rounded-full" />
             <div className="hidden lg:flex flex-row space-x-10 text-base font-semibold">
                 <Link to="/indonesia">Indonesia</Link>
                 <Link to="/programing">Programing</Link>
@@ -37,8 +53,14 @@ export const Header = ({onSidebarToogle}) => {
                 <Link to="/saved">Saved</Link>
             </div>
             <div className="flex flex-row items-center justify-center space-x-2 md:space-x-4">
-                <input type="text" className="text-black h-8 md:h-10 rounded w-44 md:w-56 px-3" name="keyword" id="search" />
-                <button className="px-2 md:px-4 h-8 md:h-10 text-white bg-red-500 hover:bg-blue-500 rounded">
+                <input 
+                    type="text" className="text-black h-8 md:h-10 rounded w-36 md:w-56 px-3"
+                    id="search" 
+                    onChange={(e)=> handleChange(e.target.value)} 
+                    value={keywords} 
+                    onKeyDown={e => e.code === 'Enter' && handleSearchNews()} 
+                />
+                <button type="button" onClick={handleSearchNews} className="px-2 md:px-4 h-8 md:h-10 text-white bg-red-500 hover:bg-blue-500 rounded">
                     <p>Search</p>
                 </button>
             </div>
@@ -51,3 +73,5 @@ export const Header = ({onSidebarToogle}) => {
         </div>
     )
 }
+
+export default Header;
