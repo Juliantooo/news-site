2335d51e12e0d602b77dfce68cea4a2efb73cf96
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-20 19:24:53 +0700

    modify class

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="bg-gray-800 p-8 flex flex-row justify-between">
+        <div className="bg-gray-800 p-6 flex flex-row justify-between items-center absolute bottom-0 w-full ">
             <p className="text-2xl font-medium">GNews</p>
             <small>Copyright © 2021 GNews. All rights reserved.</small>
         </div>
