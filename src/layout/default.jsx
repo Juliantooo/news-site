1512b666be0ba1e155085bc27c029ba304c6db83
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-20 19:29:09 +0700

    modify structure and class

--- src/layout/default.jsx
+++ src/layout/default.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import {SideNav} from '../components/sideNav';
-import {Header} from '../components/header';
-import Footer from '../components/footer';
+import { Footer, Header, SideNav } from '../components';
 
 export const Default = ({children}) => {
     const [isSideBarOpen, setIsSideBarOpen] = useState(false);
@@ -11,12 +9,12 @@ export const Default = ({children}) => {
     }
 
     return (
-        <div className="min-h-screen bg-gray-900 text-gray-200">
+        <div className="bg-gray-900 text-gray-200 h-full min-h-screen relative">
             {
                 isSideBarOpen && <SideNav onSidebarToogle={handleSideBarToggle}/>
             }
             <Header onSidebarToogle={handleSideBarToggle} />
-            <div className="w-full md:w-10/12 xl:w-9/12 mx-auto pt-5 pb-20 min-h-screen">
+            <div className="w-full md:w-10/12 xl:w-9/12 mx-auto pt-5 pb-40 tracking-wide">
             {children}
             </div>
             <Footer/>
