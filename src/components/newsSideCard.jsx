f0cd7806887a8403cb3027a7139f5343556a8a42
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-20 19:27:58 +0700

    add feat detail news

--- src/components/newsSideCard.jsx
+++ src/components/newsSideCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const NewsSideCard = ({image,title,category}) => {
+const NewsSideCard = ({image,title,category,onClickCard}) => {
     return (
-        <div className="w-full p-2 flex flex-row lg:flex-col space-x-2 lg:space-x-0 justify-between  bg-gray-600 bg-opacity-20 cursor-pointer hover:bg-opacity-50">
+        <div 
+            className="w-full p-2 flex flex-row lg:flex-col space-x-2 lg:space-x-0 justify-between  bg-gray-600 bg-opacity-20 cursor-pointer hover:bg-opacity-50"
+            onClick={()=>onClickCard(title)}
+        >
             <img src={image} alt="news" className="h-20 w-28 lg:w-full md:object-cover lg:object-contain" />
             <div className="flex flex-col justify-between flex-grow">
                 <p className="tracking-wider text-sm mt-1">{title}</p>
