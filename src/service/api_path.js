1e139e1840b8465a4ffc65fe752c6b3320300a8f
Fajar Julianto <slashcrackdie@gmail.com>
2021-11-20 19:32:27 +0700

    add search api path

--- src/service/api_path.js
+++ src/service/api_path.js
@@ -3,3 +3,4 @@ export const TOP_HEADLINES = `top-headlines?country=${region}`;
 export const COVID_19 = `everything?q=covid`;
 export const PROGRAMMING_NEWS = `everything?q=programming`;
 export const INDONESIA_NEWS = `top-headlines?country=${region}`
+export const SEARCH_NEWS = (keywords) => `everything?q=${keywords}`;
